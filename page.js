bc2ad7f4a7de68875d14f7431056b7f80272fa4b
frog89 <augustin.frank@gmail.com>
2026-08-23 18:03:38 +0200

    404.html wieder gelöscht und _redirects File wieder zugefügt

--- page.js
+++ page.js
@@ -203,20 +203,8 @@ function loadSidebar() {
 
 // Beim Start aufrufen
 window.addEventListener('DOMContentLoaded', () => {
-  const redirectUrl = sessionStorage.getItem('redirectUrl');
-  let path = '';
-
-  if (redirectUrl) {
-    sessionStorage.removeItem('redirectUrl');
-    // Entfernt führende Slashes
-    path = redirectUrl.replace(/^\/+|\/+$/g, '');
-    
-    // Setzt die schöne URL in der Browserzeile wieder ein
-    history.replaceState({ pageUrl: path }, '', '/' + path);
-  } else {
-    // Normaler Aufruf über pathname
-    path = window.location.pathname.substring(1);
-  }
+  // Liest den Pfad direkt aus der Browserzeile ab
+  let path = window.location.pathname.substring(1);
 
   if (!path || path === '' || path === 'index.html') {
     path = 'main-content.html';
